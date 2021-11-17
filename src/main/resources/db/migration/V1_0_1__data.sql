a32dde8f4caddfadaa1dcf20ebca2c825eebf55c
d_hirayama <d_hirayama@acesystems.co.jp>
2021-11-17 17:03:52 +0900

    port8100で起動するよう変更 初期データが入るよう変更

---
+++ src/main/resources/db/migration/V1_0_1__data.sql
@@ -0,0 +1,3 @@
+insert into employee(id, name, code, last_update_datetime, last_update_employee_id) values (91, '福沢', 'u3835', '2020-3-5 16:21:00', 1);
+insert into employee(id, name, code, last_update_datetime, last_update_employee_id) values (92, '新渡戸', 'u3862', '2020-3-5 16:21:00', 1);
+insert into employee(id, name, code, last_update_datetime, last_update_employee_id) values (93, '夏目', 'u3867', '2020-3-5 16:21:00', 1);
